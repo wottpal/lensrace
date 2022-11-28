187520994ac3f9a4e3fbc5b347dd8457d1bc4be1
Felix Vemmer <felix.vemmer@gmail.com>
2022-11-28 11:27:45 +0000

    feat/fontend: creates set up page

---
+++ packages/frontend/src/pages/setup.tsx
@@ -0,0 +1,15 @@
+import { CenterBody } from '@components/layout/CenterBody'
+import type { NextPage } from 'next'
+import 'twin.macro'
+
+const HomePage: NextPage = () => {
+  return (
+    <>
+      <CenterBody>
+        <div tw="text-2xl">👀 🔜 🏎️</div>
+      </CenterBody>
+    </>
+  )
+}
+
+export default HomePage
