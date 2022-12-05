8e985c339c532cde9332b9f21e63911c67c9a024
Dennis Zoma <3930150+wottpal@users.noreply.github.com>
2022-12-05 20:44:24 +0100

    frontend: Link hero heading back to start

--- packages/frontend/src/components/shared/Hero.tsx
+++ packages/frontend/src/components/shared/Hero.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import { FC } from 'react'
 import 'twin.macro'
 import raceFlag from '../../../public/images/raceFlag.svg'
@@ -6,8 +7,10 @@ import raceFlag from '../../../public/images/raceFlag.svg'
 export const Hero: FC = () => {
   return (
     <div tw="flex flex-col items-center space-y-4 py-12">
-      <Image src={raceFlag} alt="lensrace.xyz" />
-      <h1 tw="font-bold font-mono text-4xl text-base-content">lensrace.xyz</h1>
+      <Link href="/" tw="flex flex-col items-center space-y-4">
+        <Image src={raceFlag} alt="lensrace.xyz" />
+        <h1 tw="font-bold font-mono text-4xl text-base-content">lensrace.xyz</h1>
+      </Link>
       <h2 tw="text-base-content/70">on-chain follower competitions on lens protocol</h2>
     </div>
   )
