9370b6c399d6992888f02a69ba9e189e5c79ac76
feliche93 <felix.vemmer@gmail.com>
2022-12-06 16:54:54 +0100

    frontend/feat: basic working single select

--- packages/frontend/src/components/shared/InputCombobox.tsx
+++ packages/frontend/src/components/shared/InputCombobox.tsx
@@ -1,7 +1,9 @@
+import { useLazyQuery } from '@apollo/client'
 import { Combobox } from '@headlessui/react'
 import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
-import Image from 'next/image'
-import { useState } from 'react'
+import { SEARCH_PROFILES_BY_HANDLE } from '@shared/lensApiQueries'
+import debounce from 'lodash.debounce'
+import { useCallback, useEffect, useState } from 'react'
 
 import { FC } from 'react'
 import 'twin.macro'
@@ -34,21 +36,38 @@ export interface InputComboBoxProps {
 }
 
 type Profile = {
-  id: number
-  lensHandle: string
-  imageUrl: string
+  profileId: string
+  handle: string
 }
 
 export const InputComboBox: FC<InputComboBoxProps> = ({ disabled }) => {
   const [query, setQuery] = useState('')
   const [selectedProfile, setSelectedProfile] = useState(null)
 
-  const filteredprofiles =
-    query === ''
-      ? profiles
-      : profiles.filter((profile) => {
-          return profile.lensHandle.toLowerCase().includes(query.toLowerCase())
-        })
+  const [search, { data, loading, error }] = useLazyQuery(SEARCH_PROFILES_BY_HANDLE)
+  const debouncedSearch = useCallback(
+    debounce(() => {
+      if (!query) return
+      console.log(`Searching ${query}…`)
+      search({
+        variables: {
+          request: {
+            query,
+            limit: 10,
+            type: 'PROFILE',
+          },
+        },
+      })
+    }, 1000),
+    [query],
+  )
+  useEffect(() => {
+    debouncedSearch()
+  }, [query])
+
+  // console.log({ query })
+  console.log(data?.search?.items)
+  // console.log({ debouncedSearch })
 
   return (
     <Combobox as="div" value={selectedProfile} onChange={setSelectedProfile} disabled={disabled}>
@@ -61,17 +80,17 @@ export const InputComboBox: FC<InputComboBoxProps> = ({ disabled }) => {
           ]}
           // tw="w-full rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 shadow-sm focus:(border-primary outline-none ring-1 ring-primary) sm:text-sm"
           onChange={(event) => setQuery(event.target.value)}
-          displayValue={(profile: Profile) => profile?.lensHandle}
+          displayValue={(profile: Profile) => profile?.handle}
         />
         <Combobox.Button tw="absolute inset-y-0 right-0 flex items-center rounded-r-md px-2 focus:outline-none">
           <ChevronUpDownIcon tw="h-5 w-5 text-gray-400" aria-hidden="true" />
         </Combobox.Button>
 
-        {filteredprofiles.length > 0 && (
+        {!loading && !!data?.['search']?.['items']?.length && (
           <Combobox.Options tw="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-            {filteredprofiles.map((profile) => (
+            {data?.['search']?.['items'].map((profile: Profile) => (
               <Combobox.Option
-                key={profile.id}
+                key={profile?.profileId}
                 value={profile}
                 css={[
                   tw`relative cursor-default select-none py-2 pl-3 pr-9`,
@@ -88,15 +107,16 @@ export const InputComboBox: FC<InputComboBoxProps> = ({ disabled }) => {
                 {({ active, selected }) => (
                   <>
                     <div tw="flex items-center">
-                      <Image
-                        src={profile.imageUrl}
+                      {/* TODO: In lens test env there are many different urls so not sure if we can grap profile pic */}
+                      <img
+                        src={profile?.picture?.original?.url}
                         width={50}
                         height={50}
                         alt=""
                         tw="h-6 w-6 shrink-0 rounded-full"
                       />
                       <span css={[tw`ml-3 truncate`, selected && tw`font-semibold`]}>
-                        {profile.lensHandle}
+                        {profile?.handle}
                       </span>
                     </div>
 
