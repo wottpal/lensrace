7527594a7a1e061922a3b21f579c0098e9960b96
feliche93 <felix.vemmer@gmail.com>
2022-12-28 17:19:01 +0100

    feat: adds buttons

--- packages/frontend/src/pages/race/[id].tsx
+++ packages/frontend/src/pages/race/[id].tsx
@@ -1,5 +1,8 @@
 import { CenterBody } from '@components/layout/CenterBody'
+import { BaseButton, BaseButtonGroup } from '@components/shared/BaseButton'
 import { Hero } from '@components/shared/Hero'
+import { TrophyIcon } from '@heroicons/react/20/solid'
+import { ShareIcon } from '@heroicons/react/24/solid'
 import type { GetServerSideProps, GetServerSidePropsContext, NextPage } from 'next'
 import 'twin.macro'
 
@@ -9,6 +12,21 @@ const HomePage: NextPage = () => {
       <CenterBody>
         <Hero />
         {/* Button actions */}
+        <BaseButtonGroup tw="mt-4">
+          <BaseButton disabled={false} type="submit">
+            <div tw="flex items-center space-x-4">
+              <span>Share on Lens</span>
+              <ShareIcon tw="h-5 w-5" />
+            </div>
+          </BaseButton>
+          {/* TODO: Refactor tw into variant */}
+          <BaseButton tw="bg-secondary text-secondary-content" disabled={false} type="submit">
+            <div tw="flex items-center space-x-4">
+              <span>Settle</span>
+              <TrophyIcon tw="h-5 w-5" />
+            </div>
+          </BaseButton>
+        </BaseButtonGroup>
         {/* Race details */}
         {/* Leaderboard */}
       </CenterBody>
